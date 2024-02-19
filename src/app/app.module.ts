5ed7d56c9e2702efad5e9735eb20f6c5ff90be32
rcamarano <rogeriocamarano@gmail.com>
2024-02-19 12:27:31 -0300

    shipping component created, shipping prices still not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+
+
 
 
 @NgModule({
@@ -22,6 +25,8 @@ import { CartComponent } from './cart/cart.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shipping',
@@ -12,6 +15,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './shipping.component.html',
   styleUrl: './shipping.component.css'
 })
-export class ShippingComponent {
+export class ShippingComponent implements OnInit {
+    shippingCosts!: Observable<{ type: string, price: number }[]>;
 
+  ngOnInit(): void {
+    this.shippingCosts =  this.cartService.getShippingPrices();
+  }
+  constructor(private cartService: CartService) { }
 }
